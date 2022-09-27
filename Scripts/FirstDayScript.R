d91012ebe8163f39882636083f8a1057e641db51
Camden Oathout <camden.oathout@selu.edu>
2022-09-27 18:49:58 +0000

    Adding first draft of code and data

---
+++ Scripts/FirstDayScript.R
@@ -0,0 +1,5 @@
+mass <- 47.5 # Mass of animal
+age <- 122 # Age of animal
+mass_index <- mass/age #An important index
+# pound sign let's you add notes to know why you did something later
+install.packages("tidyverse")
